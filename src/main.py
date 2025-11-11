e8b7ec66efc8ba99b35f8e98c95ed1f34f8019df
Ruth Frankfort <s2169121@online.houstonisd.org>
2025-11-11 11:29:43 -0600

    Delete src directory

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from analysis import analyzer
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python src/main.py /path/to/logs_or_flashdrive")
-        sys.exit(1)
-    path = sys.argv[1]
-    analyzer.run_analysis(path)
